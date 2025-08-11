20acac2752f9a55e4556521d3fbff89f30baacef
Roham <jabbari.roham@yahoo.com>
2025-08-12 00:19:34 +0200

    next conf

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const repo = 'my-portfolio'; // <-- change to your repo name
+module.exports = {
+    output: 'export',
+    basePath: `/${repo}`,
+    assetPrefix: `/${repo}/`,
+    images: { unoptimized: true },
 };
-
-export default nextConfig;
